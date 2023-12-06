695b47e7ccc65143b2ee2ce090b2cd8114b1c527
Thibault <thibault.nguyen26@gmail.com>
2023-12-06 22:57:42 +0100

    last push

--- cpp03/ex02/src/FragTrap.cpp
+++ cpp03/ex02/src/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 21:26:43 by thibault          #+#    #+#             */
-/*   Updated: 2023/12/06 16:09:05 by thibault         ###   ########.fr       */
+/*   Updated: 2023/12/06 22:54:59 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,6 @@ FragTrap& FragTrap::operator=(const FragTrap& other) {
 
 FragTrap::~FragTrap() {
     std::cout << "FragTrap named " << _name << " destroyed!" << std::endl;
-}
-
-void FragTrap::attack(const std::string& target) {
-    if (_hitPoints > 0 && _energyPoints > 0) {
-		std::cout << "FragTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
-		_energyPoints--;
-	} else {
-		std::cout << "FragTrap " << _name << " cannot attack due to lack of hit points or energy points!" << std::endl;
-	}
 }
 
 void FragTrap::highFivesGuys() {
